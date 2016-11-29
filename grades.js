ff4a093c6ffde3d6dc9b904a964ad63bb14b54fc
LucasMcL <LMcLaughlin09@gmail.com>
2016-11-29 15:35:08 -0600

    completed assignment

--- grades.js
+++ grades.js
@@ -14,3 +14,50 @@
 // What is the lowest grade?
 // What is the highest grade?
 
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+// Initialize variables to count total number of each grade
+var numA = 0;
+var numB = 0;
+var numC = 0;
+var numD = 0;
+var numF = 0;
+
+// Initialize lowest grade and highest grade variables
+// Give them initial value of first score
+// Will work with array of length 1
+var lowGrade = scores[0];
+var highGrade = scores[0];
+
+// Loop through array
+for (var i = 0; i < scores.length; i++) {
+	// incriment counter for each grade
+	if (scores[i] >= 91) {numA++};
+	if (scores[i] >= 81 && scores[i] <= 90) {numB++};
+	if (scores[i] >= 71 && scores[i] <= 80) {numC++};
+	if (scores[i] >= 61 && scores[i] <= 70) {numD++};
+	if (scores[i] <= 60) {numF++};
+
+	// Test for new highest or new lowest grade
+	if (scores[i] > highGrade) {highGrade = scores[i]}
+	if (scores[i] < lowGrade) {lowGrade = scores[i]}
+}
+
+// Output results
+console.log("The number of As is: " + numA);
+console.log("The number of Bs is: " + numB);
+console.log("The number of Cs is: " + numC);
+console.log("The number of Ds is: " + numD);
+console.log("The number of Fs is: " + numF);
+
+console.log("The highest grade is: " + highGrade);
+console.log("The lowest grade is: " + lowGrade);
+
+
+
+
+
+
+
+
+
